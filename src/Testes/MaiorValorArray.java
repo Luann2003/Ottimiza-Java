88fa37bbe47e751d09c78687e2a8f5ae0fbae91b
Luann2003 <lvictorluciano@outlook.com>
2024-01-23 12:55:02 -0300

    correção2

--- src/Testes/MaiorValorArray.java
+++ src/Testes/MaiorValorArray.java
@@ -5,13 +5,11 @@ public class MaiorValorArray {
 	public static void main(String[] args) {
 		
 		int numbers[] = {334, 568, 123, 796, 225};
-		int maior = 0;
+		int maior = Integer.MIN_VALUE;
 		
 		
 		for(int lista : numbers) {
-			if(maior > lista) {
-				
-			}else {
+			if(maior < lista) {
 				maior = lista;
 			}
 		}
